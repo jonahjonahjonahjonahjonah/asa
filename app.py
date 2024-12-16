0327e6241eabb2024cb343ebeb57a55b477a9e73
jonahjonahjonahjonahjonah <jonah.santoso@student.sph.ac.id>
2024-12-16 10:17:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template, request, Response
 import cv2
-from gpiozero import Motor
+import time
+import threading
+# from gpiozero import Motor
 
-frontleft = Motor(3,2)
-frontright = Motor(14,15)
-backleft = Motor(17,27)
-backright = Motor(24,23)
+# frontleft = Motor(3,2)
+# frontright = Motor(14,15)
+# backleft = Motor(17,27)
+# backright = Motor(24,23)
 
 app = Flask(__name__)
+recording = False
+def record():
+    global recording
+    cap = cv2.VideoCapture(0)
+
+    frame_width = int(cap.get(3)) 
+    frame_height = int(cap.get(4)) 
+    size = (frame_width,frame_height)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    result = cv2.VideoWriter('video.avi', fourcc, 10, size)
+
+    while recording:
+        ret, frame =  cap.read()
+        print(ret)
+        result.write(frame)
+    result.release()
 
 def gen_frames(): 
     camera = cv2.VideoCapture(0)  
@@ -23,6 +41,17 @@ def gen_frames():
 def index():
     return render_template('index.html')
 
+@app.route('/record', methods=["POST"])
+def recordcontrol():    
+    global recording
+    if request.form['recording'] == 'start':
+        recording = True
+        threading.Thread(target=record).start()
+    else:
+        recording = False 
+    print("recording"+str(recording))
+    return '',204
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame') 
@@ -34,28 +63,30 @@ def control():
         print('forward')
         # frontleft.forward() 
         # frontright.forward()
-        backleft.forward()
-        backright.forward()
-
+        # backleft.forward()
+        # backright.forward()
+        
     elif direction == 'backward':
         print('backward')
         # frontleft.backward()
         # frontright.backward()
-        backleft.backward()
-        backright.backward()
+        # backleft.backward()
+        # backright.backward()
     elif direction == 'right':
-        
-        backleft.forward()
-        backright.backward()
+        print('right')
+        # backleft.forward()
+        # backright.backward()
     elif direction == 'left':
-        
-        backleft.backward()
-        backright.forward()
+        print('left')
+        # backleft.backward()
+        # backright.forward()
     elif direction == 'stop':
+        print('stop')
         # frontleft.stop()
         # frontright.stop()
-        backleft.stop()
-        backright.stop()
+        # backleft.stop()
+        # backright.stop()
+    return '',204
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
