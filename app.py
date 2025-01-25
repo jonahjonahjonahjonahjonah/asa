e93a00bca1ecf6c7249a1c1007fac28a9bd8a504
jonahjonahjonahjonahjonah <jonah.santoso@student.sph.ac.id>
2025-01-25 21:40:37 +0700

    Update app.py
    allow it to connect to api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, Response
 import cv2
 import time
 import threading
+import requests
 # from gpiozero import Motor
 
 # frontleft = Motor(3,2)
@@ -13,48 +14,42 @@ app = Flask(__name__)
 recording = False
 def record():
     global recording
-    cap = cv2.VideoCapture(0)
+
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
     frame_width = int(cap.get(3)) 
     frame_height = int(cap.get(4)) 
     size = (frame_width,frame_height)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    result = cv2.VideoWriter('video.avi', fourcc, 10, size)
+    result = cv2.VideoWriter('video.avi', fourcc, 30, size)
 
     while recording:
-        ret, frame =  cap.read()
-        print(ret)
-        result.write(frame)
+        success, frame =  cap.read()
+        print(success)
+
+        if success == True:
+            result.write(frame)
+            
     result.release()
+    print("recording stopped")
+
+    while not recording:
+        time.sleep(0.1)
 
 def gen_frames(): 
-    camera = cv2.VideoCapture(0)  
+    camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)  
 
     while True:
         ret, frame = camera.read()  
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n' 
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+        if ret == True:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n' 
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/record', methods=["POST"])
-def recordcontrol():    
-    global recording
-    if request.form['recording'] == 'start':
-        recording = True
-        threading.Thread(target=record).start()
-    else:
-        recording = False 
-    print("recording"+str(recording))
-    return '',204
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame') 
 
 @app.route('/control', methods=['POST'])
 def control():
@@ -87,6 +82,45 @@ def control():
         # backleft.stop()
         # backright.stop()
     return '',204
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame') 
+
+@app.route('/record', methods=["POST"])
+def recordcontrol():    
+    global recording
+    if request.form['recording'] == 'start':
+        recording = True
+        threading.Thread(target=record).start()
+    else:
+        recording = False 
+    print("recording"+str(recording))
+    return '',204
+
+@app.route('/train', methods=["POST"])
+def train():   
+    if request.form['train'] == 'train':
+        # upload the video to api
+
+        url = 'http://127.0.0.1:5000/upload' # or some other url
+        file_path = 'test.txt'
+
+        with open(file_path, 'rb') as file:
+            files = {'file': file}
+            response = requests.post(url, files=files)
+
+        print(response.json())
+
+        # process and train and viewer url
+        url = 'http://127.0.0.1:5000/train'
+        response = requests.post(url)
+        print(response.json())  
+        
+    return '',204
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
